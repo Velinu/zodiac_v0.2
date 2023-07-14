f22014dd573bd6a3398186b8b2419568f3df5f32
Velinu <matheusfelintosilva2@gmail.com>
2023-07-14 11:07:23 -0300

    first commit

---
+++ src/components/banner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="banner-container" :image="url( )">
+        <img :src="banner" :alt="alt" />
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "bannerTemplate",
+        props: {
+            banner: String,
+            alt: String
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
